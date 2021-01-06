c367d4936c49ee71deb7d9e98106a9ca8cc352ce
Mateus Carnevalli Terni <mcterni@yahoo.com>
2021-01-06 17:05:35 -0500

    Basic responsive grid layout

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchBox from './components/SearchBox';
 import ResultList from './components/ResultList';
+import {Container, Row, Col} from 'react-bootstrap'
 
 
 function App() {
   const [searchFor, setSearchFor] = useState('');
   return (
-    <div className="App container">
+    <Container className="App" fluid = 'md'>
       <header>
         <h1>Welcome to My Favorite Movies</h1>
       </header>
+      
       <SearchBox onChange={(e) => setSearchFor(e.target.value)} />
-      <ResultList searchFor={searchFor} />
 
-    </div>
+      <Row xs = {1} md = {2}>
+        <Col>
+          Result List
+          <ResultList searchFor={searchFor} />           
+        </Col>
+        <Col>List of Favorite Movies
+        </Col>
+      </Row>
+
+    </Container>
   );
 }
 
