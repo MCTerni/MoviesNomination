8d5d21c3f8692da6888a2b6c0c9e95085496e658
Mateus Carnevalli Terni <mcterni@yahoo.com>
2021-01-06 21:19:43 -0500

    Adding movies to favorite movies list

---
+++ movie-app/src/components/FavoriteList.js
@@ -0,0 +1,32 @@
+import { useState, useEffect  } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+
+
+function FavoriteList(props) {
+    const [list, setList] = useState([]);
+    // const [listSize, setListSize] = useState(0);
+
+    useEffect(() =>{
+        if(props.movie){
+            let tempList = list;
+            tempList.push(props.movie)
+            setList(tempList);
+            // setListSize(listSize+1);
+        }
+    },[props.movie]);
+
+    return (
+        <ListGroup>                
+            { list.map((element) => {
+                return (
+                        <ListGroup.Item key = {element}>
+                            {element}
+                        </ListGroup.Item>   
+                );
+            }) }
+            
+        </ListGroup>
+    );
+}
+
+export default FavoriteList;
